3b8d2aa76e8dd9e61be550a13a55391db928a292
Reid Minton <reid.minton@icloud.com>
2025-11-23 17:09:19 -0600

    Frontend UI completed

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,107 @@
+"use client"
+
+import * as React from "react"
+import {
+  AudioWaveform,
+  Command,
+  ListMusic,
+  GalleryVerticalEnd,
+  UserCheck,
+} from "lucide-react"
+
+import { NavMain } from "@/components/nav-main"
+import { NavUser } from "@/components/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+  SidebarMenuButton,
+} from "@/components/ui/sidebar"
+
+// This is sample data.
+const data = {
+  user: {
+    name: "reidm",
+    email: "reid.minton@utdallas.edu",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "Acme Inc",
+      logo: GalleryVerticalEnd,
+      plan: "Enterprise",
+    },
+    {
+      name: "Acme Corp.",
+      logo: AudioWaveform,
+      plan: "Startup",
+    },
+    {
+      name: "Evil Corp.",
+      logo: Command,
+      plan: "Free",
+    },
+  ],
+  navMain: [
+    {
+      title: "Personal",
+      url: "#",
+      icon: ListMusic,
+      isActive: true,
+      items: [
+        {
+          title: "Liked Songs",
+          url: "#",
+        },
+        {
+          title: "My Playlists",
+          url: "#",
+        },
+        {
+          title: "Favorites",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Following",
+      url: "#",
+      icon: UserCheck,
+      items: [
+        {
+          title: "Focus Playlist",
+          url: "#",
+        },
+      ],
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <SidebarMenuButton
+          size="lg"
+          className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+        >
+          <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+            <AudioWaveform className="size-4" />
+          </div>
+          <div className="grid flex-1 text-left text-sm leading-tight">
+            <span className="truncate font-medium">Libraries</span>
+          </div>
+        </SidebarMenuButton>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
